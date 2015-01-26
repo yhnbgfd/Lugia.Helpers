086dbd070a630cc487121689f9f6e387658abaca
HuangShuhang_i5 <yhnbgfd@gmail.com>
2015-01-26 16:40:23 +0800

    + SQLite dll

---
+++ HelpersClassLibrary/Helpers/Database/SQLite/SQLiteHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+
+namespace Lugia.Helpers.Database.SQLite
+{
+    class SQLiteHelper
+    {
+        private SQLiteConnection _connection = null;
+
+    }
+}
