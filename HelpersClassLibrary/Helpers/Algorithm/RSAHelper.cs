269da8196099b0a1ce9b1cda4bdb1641339bf42c
HuangShuhang_i5 <yhnbgfd@gmail.com>
2015-01-31 19:29:44 +0800

    完善rsa

--- HelpersClassLibrary/Helpers/Algorithm/RSAHelper.cs
+++ HelpersClassLibrary/Helpers/Algorithm/RSAHelper.cs
@@ -6,47 +6,76 @@ namespace Lugia.Helpers.Algorithm
 {
     /// <summary>
     /// RSA工具类
+    /// 出于安全的考虑，RSACryptoServiceProvider中Encrypt、Decrypt方法只支持公钥加密、私钥解密，用途是数据加密
+    /// 而私钥加密、公钥解密的使用途径是数字签名，内容验证，对应RSACryptoServiceProvider中的SignData、VerifyData
     /// </summary>
     public class RSAHelper
     {
         /// <summary>
-        /// 加密
+        /// 产生公钥(1)和私钥(0)对
         /// </summary>
-        /// <param name="publicKey"></param>
-        /// <param name="content"></param>
+        /// <param name="dwKeySize">The size of the key to use in bits.</param>
         /// <returns></returns>
-        public string Encrypt(string publicKey, string content)
+        public string[] CreateKey(int dwKeySize = 1024)
         {
-            return this.Encrypt(publicKey, Encoding.UTF8.GetBytes(content));
+            string[] keys = new string[2];
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(dwKeySize);
+            keys[0] = rsa.ToXmlString(true);//私钥（私钥里面前半段为公钥）
+            keys[1] = rsa.ToXmlString(false);//公钥
+            return keys;
         }
+
+        #region 公钥加密、私钥解密
         /// <summary>
-        /// 加密
+        /// 公钥加密
         /// </summary>
-        /// <param name="publicKey"></param>
-        /// <param name="content"></param>
+        /// <param name="publicKey">公钥</param>
+        /// <param name="data">原数据</param>
         /// <returns></returns>
-        public string Encrypt(string publicKey, byte[] content)
+        public string Encrypt(string publicKey, string data)
+        {
+            return this.Encrypt(publicKey, Encoding.UTF8.GetBytes(data));
+        }
+
+        /// <summary>
+        /// 公钥加密
+        /// </summary>
+        /// <param name="publicKey">公钥</param>
+        /// <param name="data">原数据</param>
+        /// <returns></returns>
+        public string Encrypt(string publicKey, byte[] data)
         {
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             rsa.FromXmlString(publicKey);
-            byte[] cipherbytes = rsa.Encrypt(content, false);
+            byte[] cipherbytes = rsa.Encrypt(data, false);
             return Convert.ToBase64String(cipherbytes);
         }
 
         /// <summary>
-        /// 解密
+        /// 私钥解密
+        /// </summary>
+        /// <param name="privateKey">私钥</param>
+        /// <param name="data">原数据</param>
+        /// <returns></returns>
+        public string Decrypt(string privateKey, string data)
+        {
+            return this.Decrypt(privateKey, Convert.FromBase64String(data));
+        }
+
+        /// <summary>
+        /// 私钥解密
         /// </summary>
         /// <param name="privateKey"></param>
-        /// <param name="content"></param>
+        /// <param name="data"></param>
         /// <returns></returns>
-        public string Decrypt(string privateKey, string content)
+        public string Decrypt(string privateKey, byte[] data)
         {
             RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
             rsa.FromXmlString(privateKey);
             byte[] cipherbytes = null;
             try
             {
-                cipherbytes = rsa.Decrypt(Convert.FromBase64String(content), false);
+                cipherbytes = rsa.Decrypt(data, false);
             }
             catch (System.Security.Cryptography.CryptographicException)
             {
@@ -54,18 +83,59 @@ namespace Lugia.Helpers.Algorithm
             }
             return Encoding.UTF8.GetString(cipherbytes);
         }
+        #endregion
+
+        #region 私钥加密、公钥解密
+        /// <summary>
+        /// 私钥加密
+        /// </summary>
+        /// <param name="privateKey">私钥</param>
+        /// <param name="data">原数据</param>
+        /// <returns></returns>
+        public string SignData(string privateKey, byte[] data)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(privateKey);
+            return Convert.ToBase64String(rsa.SignData(data, "MD5"));
+        }
 
         /// <summary>
-        /// 产生公钥和私钥对
+        /// 私钥加密
         /// </summary>
-        /// <returns>string[] 0:私钥;1:公钥</returns>
-        public string[] CreateKey()
+        /// <param name="privateKey"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public string SignData(string privateKey, string data)
         {
-            string[] keys = new string[2];
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            keys[0] = rsa.ToXmlString(true);//私钥
-            keys[1] = rsa.ToXmlString(false);//公钥
-            return keys;
+            return SignData(privateKey, Encoding.UTF8.GetBytes(data));
         }
+
+        /// <summary>
+        /// 公钥解密
+        /// </summary>
+        /// <param name="publicKey">公钥</param>
+        /// <param name="data">原数据</param>
+        /// <param name="signature">证书数据</param>
+        /// <returns></returns>
+        public bool VerifyData(string publicKey, byte[] data, byte[] signature)
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(publicKey);
+            return rsa.VerifyData(data, "MD5", signature);
+        }
+
+        /// <summary>
+        /// 公钥解密
+        /// </summary>
+        /// <param name="publicKey"></param>
+        /// <param name="data"></param>
+        /// <param name="signature"></param>
+        /// <returns></returns>
+        public bool VerifyData(string publicKey, string data, string signature)
+        {
+            return VerifyData(publicKey, Encoding.UTF8.GetBytes(data), Encoding.UTF8.GetBytes(signature));
+        }
+
+        #endregion
     }
 }
--- TestApp/Views/Pages/Algorithms/RSA/Page_RSA.xaml.cs
+++ TestApp/Views/Pages/Algorithms/RSA/Page_RSA.xaml.cs
@@ -17,7 +17,8 @@ namespace TestApp.Views.Pages.Algorithms.RSA
 
         private void Button_CreateKey_Click(object sender, RoutedEventArgs e)
         {
-            _keys = new RSAHelper().CreateKey();
+            int dwKeySize = int.Parse(((ComboBoxItem)this.ComboBox_dwKeySize.SelectedItem).Content.ToString());
+            _keys = new RSAHelper().CreateKey(dwKeySize);
             this.TextBox_Publickey.Text = _keys[1];
             this.TextBox_Privatekey.Text = _keys[0];
         }
@@ -30,6 +31,15 @@ namespace TestApp.Views.Pages.Algorithms.RSA
         private void Button_Decrypt_Click(object sender, RoutedEventArgs e)
         {
             this.TextBox_DecryptStr.Text = new RSAHelper().Decrypt(this.TextBox_Privatekey.Text, this.TextBox_EncryptResult.Text);
+        }
+
+        private void ComboBox_dwKeySize_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (!this.IsLoaded)
+            {
+                return;
+            }
+            Button_CreateKey_Click(null, null);
         }
     }
 }
